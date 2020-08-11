6589befd7fd9f8a6f2cf379708fb1946ab39a742
Jonas.Stoehr <jonas.stoehr@nhochdrei.de>
2020-08-11 11:44:10 +0200

    BUG #24221 Checkliste beim Kopieren mit übernehmen - wird jetzt uebernommen

--- lib/redmine_issue_checklist/patches/issue_patch.rb
+++ lib/redmine_issue_checklist/patches/issue_patch.rb
@@ -10,14 +10,14 @@ module RedmineIssueChecklist
       end
 
       module InstanceMethods
-        def copy_from(arg, options={})
-          super(arg, options)
-          issue          = arg.is_a?(Issue) ? arg : Issue.visible.find(arg)
-          self.checklist = issue.checklist.map { |cl| cl.dup }
-          self.checklist.each do |object|
-            object.is_done = nil
+        def copy_from(arg, options = {})
+          ret = super
+          return ret unless self.checklist.empty?
+          issue = @copied_from || (arg.is_a?(Issue) ? arg : Issue.visible.find(arg))
+          @issue_checklist_copied = issue.checklist.map do |c|
+            c.attributes.symbolize_keys.slice(:subject).merge is_done: false
           end
-          self
+          ret
         end
 
         def update_checklist_items(checklist_items, create_journal = false)
@@ -25,8 +25,12 @@ module RedmineIssueChecklist
 
           old_checklist = checklist.collect(&:info).join(', ')
 
+          existing = checklist_items.map { |c| c[:subject] }.to_set
+          copied = (@issue_checklist_copied || []).reject { |c| existing.include? c[:subject] }
+          checklist_items.unshift *copied
+
           checklist.destroy_all
-          checklist << checklist_items.uniq.collect do |cli|
+          checklist << checklist_items.collect do |cli|
             IssueChecklist.new(is_done: cli[:is_done], subject: cli[:subject])
           end
 
@@ -34,10 +38,10 @@ module RedmineIssueChecklist
 
           if current_journal && create_journal && (new_checklist != old_checklist)
             current_journal.details << JournalDetail.new(
-              property:  'attr',
-              prop_key:  'checklist',
-              old_value: old_checklist,
-              value:     new_checklist)
+                property: 'attr',
+                prop_key: 'checklist',
+                old_value: old_checklist,
+                value: new_checklist)
           end
         end
 
